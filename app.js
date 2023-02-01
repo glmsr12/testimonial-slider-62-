2fa6efcd1b438c86361a1d5b08d3a4da02e8d08e
glmsr12 <gulumsercakmak@hotmail.com>
2023-01-31 21:03:23 -0800

    "update file"

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-var btn = document.querySelector('btn');
 var slide = document.getElementById('slide');
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
 
-/*
-btn.onclick = function () {
+btn1.onclick = function () {
+  slide.style.transform = 'translateX(800px)';
+  btn1.classList.add('active');
+  btn2.classList.remove('active');
+  btn3.classList.remove('active');
+};
+btn2.onclick = function () {
   slide.style.transform = 'translateX(0px)';
-};
-btn.onclick = function () {
-  slide.style.transform = 'translateX(-800px)';
+  btn1.classList.remove('active');
+  btn2.classList.add('active');
+  btn3.classList.remove('active');
 };
 
-btn[2].onclick = function () {
-  slide.style.transform = 'translateX(-1600px)';
+btn3.onclick = function () {
+  slide.style.transform = 'translateX(-800px)';
+  btn1.classList.remove('active');
+  btn2.classList.remove('active');
+  btn3.classList.add('active');
 };
-*/
