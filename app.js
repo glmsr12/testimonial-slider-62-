77f7aa008e85f23312778907045914c69e46a312
glmsr12 <gulumsercakmak@hotmail.com>
2023-01-31 18:28:09 -0800

    "update the style"

---
+++ app.js
@@ -0,0 +1,15 @@
+var btn = document.querySelector('btn');
+var slide = document.getElementById('slide');
+
+/*
+btn.onclick = function () {
+  slide.style.transform = 'translateX(0px)';
+};
+btn.onclick = function () {
+  slide.style.transform = 'translateX(-800px)';
+};
+
+btn[2].onclick = function () {
+  slide.style.transform = 'translateX(-1600px)';
+};
+*/
